09789dbc7d038e0d716b045b448687c9353d5ee8
mmansourime <m.mansouri.me@gmail.com>
2022-07-12 17:55:38 +0430

    Some Changed

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ class Todo {
 //یه تابعی ویژه است که هر زمان یه شی از این کلاس تودو را ایجاد کنیم این تابع خود به خود فراخوانی میشود 
     constructor(todoInputID, todoListID) {
         //دوتا آرگومان برای گرفتن ای دی ورودی و لیست بهش میدیم
-    console.log(todoInputID, todoListID)
-        
+    this.todoInput = document.getElementById(todoInputID);
+    this.todoList = document.getElementById(todoListID);
+    //حالا میخواهیم بعداز اینکه المنت هامون را گرفتیم یه لیستی از تودوهامون داخل یه آرایه داشته باشیم
+    this.todos = [];
+    
+         
     }
 }
 //برای فراخوانی کلاس تودو بعد از ایجاد شی
-new Todo('todo-Input', 'todo-list');
+new Todo('todo-I 
